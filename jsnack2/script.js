8f552283a6ad6f944d0f1d9aeb86c1e8a3f0c91b
MemeWasTaken <memelazzari02@gmail.com>
2021-11-26 16:16:56 +0100

    jsnack2

---
+++ jsnack2/script.js
@@ -0,0 +1,40 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const guests = ['leonardo', 'tobey', 'carey', 'joel', 'elizabeth', 'isla', 'jason', 'amitabh']
+
+let nameUser =  prompt('Inserisci il tuo nome:').toLowerCase();
+
+let found = false;
+
+for (let i = 0; i < guests.length; i++) {
+    
+    if (nameUser == guests[i]) {
+         console.log(nameUser);
+        found = true;
+    }
+     console.log(guests[i]);
+    
+}
+
+if (found == true) {
+      console.log('Il nome è stato trovato');
+    } else {
+      console.log('Il nome non e stato trovato');
+    }
+
+console.log(found);
+
+// While Loop
+// let t = 0;
+// while (t < guests.length && found == false) {
+//     if (guests[t] == nameUser) {
+//         found = true;
+//     }
+//     t++;
+// }
+
+// if (found == true) {
+//       console.log('Il nome è stato trovato');
+//     } else {
+//       console.log('Il nome non e stato trovato');
+//     }
